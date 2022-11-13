15d792f3f9916e1303014921430277b189ba00e1
juanliu6 <2022212483@stu.cqupt.edu.cn>
2022-11-13 17:18:27 +0800

    huiwenshu

--- src/huiwenshu.java
+++ src/huiwenshu.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class huiwenshu {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class huiwenshu {
+    public static void main(String[] args) {
+        System .out .println("输入一个数");
+        Scanner sc=new Scanner(System.in);
+        int x= sc.nextInt();
+        String s=x+"";
+        char []charArr=s.toCharArray();
+        char []chars =new char[s.length() ];
+        for (int i=0;i<charArr.length ;i++ ){
+            chars [i]=charArr [charArr .length-i-1 ];
+        }
+        String s1=new String(chars );
+        System.out .println(Arrays.equals(charArr  , s1.toCharArray())) ;
+
+    }
 }
